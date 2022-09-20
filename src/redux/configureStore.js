19c094ebee40dd73c70e177ce23604b5d1069a75
aceDavon <makojidavid@gmail.com>
2022-09-20 21:18:26 +0100

    Create the redux store
    Create the redux store to hold the application state in one single point
    of truth, Combine the imported reducers into a root reducer and add to
     the store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import booksReducer from "./books/BookSlice";
+import categoryReducer from "./categories/CategorySlice";
+
+const rootReducer = combineReducers({
+  booksReducer,
+  categoryReducer
+});
+
+const Store = configureStore({
+  name: 'Books Store',
+  reducer: rootReducer,
+});
+
+export default Store;
