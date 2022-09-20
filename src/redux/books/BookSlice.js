8690114927da59962190f9d3cf9c6000cad42279
aceDavon <makojidavid@gmail.com>
2022-09-20 21:16:25 +0100

    Extract the books component to a slice
    Extract and refactor the books component to a stand alone slice file,
     refactor the books component for redux actions and then create the
     bookslice for implementing the reducers and actions for the books
     component

---
+++ src/redux/books/BookSlice.js
@@ -0,0 +1,15 @@
+export const addBook = "ADD_BOOK";
+export const removeBook = "REMOVE_BOOK";
+
+const booksReducer = (state = [], action) => {
+  switch (action.type) {
+    case addBook:
+      return state.concat(action.payload);
+    case removeBook:
+      return state.map(book => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default booksReducer;
