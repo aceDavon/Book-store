983c24014f1e24e785f192748b5fffb0597e12d2
aceDavon <makojidavid@gmail.com>
2022-09-21 10:45:49 +0100

    Add the redux store to the App
    Add the redux store through the provider from react-redux on the main App
    component for use throughout the code base

--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import BookContainer from './components/BookContainer';
 import Category from './components/Category';
+import Store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route element={<App />} path="/">
-          <Route element={<BookContainer />} index />
-          <Route element={<Category />} path="category" />
-        </Route>
-
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+  <Provider store={Store}>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route element={<App />} path="/">
+            <Route element={<BookContainer />} index />
+            <Route element={<Category />} path="category" />
+          </Route>
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  </Provider>,
 );
