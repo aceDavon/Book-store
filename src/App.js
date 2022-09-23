1918dbc9ddf0dcc84c9b66a2b0e811e5438c93bc
aceDavon <makojidavid@gmail.com>
2022-09-23 08:46:38 +0100

    Install Axios for API calls
    Install Axios for making API calls and remove checks for camel casing on
     linters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import { fetchData } from './redux/books/BookSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
   return (
     <div className="bg w-11/12 lg:w-10/12 mx-auto my-10 shadow shadow-gray-400">
       <NavBar />
